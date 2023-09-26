9c0699e2afbe1fb76a4f5f2686d72298ea60b624
Malbora Roci <boraroci2019@gmail.com>
2023-09-26 17:00:31 +0200

    first commit

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from './Hero';
+import PopularProducts from './PopularProducts';
+import SuperQuality from './SuperQuality';
+import Services from './Services';
+import SpecialOffer from './SpecialOffer';
+import Footer  from './Footer';
+import Subscribe from './Subscribe'
+import CustomerReviews from './CustomerReviews'
+
+export {
+    Hero,
+    PopularProducts,
+    SuperQuality,
+    Services,
+    SpecialOffer,
+    Footer,
+    Subscribe,
+    CustomerReviews
+}
